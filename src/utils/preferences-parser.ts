fbb1bb22382eeadcc340379b8cf3ea6f649d9502
Dennis Christoffersen <dennis@fantastisk.dk>
2019-10-28 17:35:38 +0100

    refactor: avalilibility is now calculated from avalilibility array which is parsed in

--- src/utils/preferences-parser.ts
+++ src/utils/preferences-parser.ts
@@ -1,22 +1,15 @@
 import {IPreferences} from "../model/preferences";
+import {IAvailability} from "../types/Availability";
 
-export function findAvailbleInterval( preferences:IPreferences ):{min:number, max:number} {
 
-	const daysMap = ["monday", "tuesday", "wednesday", "thursday", "friday"];
+export function findAvailbleInterval( availability:IAvailability[] ):{min:number, max:number} {
 
-	const availableInterval = daysMap.map( (day, index) => {
-		//@ts-ignore
-		return preferences[day]
-	})
-		.reduce( (a, value) => {
+	return availability.reduce( (a, value) => {
 			const min = Math.min(a.min, value.startTimeInMinutes )
 			const max = Math.max(a.max, value.endTimeInMinutes )
 			return {min: min, max:max }
 
 	},{min:1500, max:-1});
-
-	return availableInterval;
-
 
 }
 
